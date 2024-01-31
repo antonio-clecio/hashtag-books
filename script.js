654083b912b1bc2d6c6e87c43469c654b72d269b
Antonio Clecio <clecio_devops@outlook.com>
2024-01-31 20:05:13 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,34 @@
+const botaoPlayPause = document.getElementById('play-pause');
+const audioCapitulo = document.getElementById('audio-capitulo');
+
+const numeroCapitulos = 10;
+let taTocando = 0;
+let capituloAtual = 1;
+
+function tocarFaixa() {
+  audioCapitulo.play();
+  botaoPlayPause.classList.remove('bi-play-circle-fill');
+  botaoPlayPause.classList.add('bi-pause-circle-fill');
+}
+
+function pausarFaixa() {
+  audioCapitulo.pause();
+  botaoPlayPause.classList.add('bi-play-circle-fill');
+  botaoPlayPause.classList.remove('bi-pause-circle-fill');
+}
+
+function tocarOuPausar() {
+  if(taTocando === 0) {
+    tocarFaixa();
+    taTocando = 1;
+  } else {
+    pausarFaixa();
+    taTocando = 0;
+  }
+}
+
+function proximaFaixa() {
+  capituloAtual++
+}
+
+botaoPlayPause.addEventListener('click', tocarOuPausar);
