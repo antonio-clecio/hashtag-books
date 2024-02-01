73d4062143271e0332f866080f74d092b009b5b8
Antonio Clecio <clecio_devops@outlook.com>
2024-02-01 14:24:55 -0300

    second commit

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
+const nomeCapitulo = document.getElementById('capitulo');
 const botaoPlayPause = document.getElementById('play-pause');
+const botaoAvancar = document.getElementById('proximo');
+const botaoVoltar = document.getElementById('anterior');
 const audioCapitulo = document.getElementById('audio-capitulo');
 
 const numeroCapitulos = 10;
 let taTocando = 0;
 let capituloAtual = 1;
+
+function trocarNomeFaixa() {
+  nomeCapitulo.innerText = 'Capítulo ' + capituloAtual;
+}
+
 
 function tocarFaixa() {
   audioCapitulo.play();
@@ -28,7 +36,31 @@ function tocarOuPausar() {
 }
 
 function proximaFaixa() {
-  capituloAtual++
+  if(capituloAtual === numeroCapitulos) {
+    capituloAtual = 1;
+  } else {
+    capituloAtual++
+  }
+
+  audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+  tocarFaixa();
+  taTocando = 1;
+  trocarNomeFaixa();
+}
+
+function voltarFaixa() {
+  if(capituloAtual === 1) {
+    capituloAtual = numeroCapitulos;
+  } else {
+    capituloAtual--
+  }
+
+  audioCapitulo.src = "./books/dom-casmurro/" + capituloAtual + ".mp3";
+  tocarFaixa();
+  taTocando = 1;
+  trocarNomeFaixa();
 }
 
 botaoPlayPause.addEventListener('click', tocarOuPausar);
+botaoAvancar.addEventListener('click', proximaFaixa);
+botaoVoltar.addEventListener('click', voltarFaixa);
